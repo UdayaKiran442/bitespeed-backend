50413ad9ca428939e4f09d635293abc0d06df0af
UdayaKiran442 <gudaya2002@gmail.com>
2025-07-01 16:07:55 +0530

    TASK: add health check url

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import identifyRoute from './routes/identify.route'
 
 const app = new Hono()
 
+app.get("/health-check", (c) => {
+  return c.json({ message: "Service is running" })
+})
+
 app.route('/identify', identifyRoute)
 
 export default app
